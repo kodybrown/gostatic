8c16d62bdca0682557bc127e8a2cd105fbb1089b
Kody Brown <thewizard@wasatchwizard.com>
2017-02-17 22:13:46 -0700

    Removed sublime project

--- version.go
+++ version.go
@@ -4,6 +4,6 @@ package main
 
 var (
     buildVersion string = "1.1"
-    buildDate string = "2017-01-24 11:02:31"
-    buildVersionStr string = "v1.1.170124.1102"
+    buildDate string = "2017-02-17 22:13:32"
+    buildVersionStr string = "v1.1.170217.2213"
 )
